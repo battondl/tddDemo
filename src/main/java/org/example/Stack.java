5b161128e1592af321e2f099ba4bb7d14210e16b
David L. Batton <david.batton@outlook.com>
2023-03-31 09:42:13 -0500

    Solution Complete

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -2,29 +2,37 @@ package org.example;
 
 public class Stack {
 
-    Object top;
+    private class Node {
+        public Object value;
+        public Node next;
+    }
+
+    Node top;
     public boolean isEmpty() {
 
         return top == null;
     }
 
     public void push(Object item) {
-        top = item;
+        Node newNode = new Node();
+        newNode.value = item;
+        newNode.next = top;
+        top = newNode;
     }
 
     public Object peek() throws StackEmptyException {
         if(isEmpty()) {
             throw new StackEmptyException();
         }
-        return top;
+        return top.value;
     }
 
     public Object pop() {
         if(isEmpty()) {
             return null;
         }
-        Object temp = top;
-        top = null;
-        return temp;
+        Node temp = top;
+        top = top.next;
+        return temp.value;
     }
 }
