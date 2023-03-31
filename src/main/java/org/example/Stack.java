094084d58353b1bb6e0132081f0bbe72ce0acfa7
David L. Batton <david.batton@outlook.com>
2023-03-31 09:59:42 -0500

    start init

--- src/main/java/org/example/Stack.java
+++ src/main/java/org/example/Stack.java
@@ -2,37 +2,5 @@ package org.example;
 
 public class Stack {
 
-    private class Node {
-        public Object value;
-        public Node next;
-    }
 
-    Node top;
-    public boolean isEmpty() {
-
-        return top == null;
-    }
-
-    public void push(Object item) {
-        Node newNode = new Node();
-        newNode.value = item;
-        newNode.next = top;
-        top = newNode;
-    }
-
-    public Object peek() throws StackEmptyException {
-        if(isEmpty()) {
-            throw new StackEmptyException();
-        }
-        return top.value;
-    }
-
-    public Object pop() {
-        if(isEmpty()) {
-            return null;
-        }
-        Node temp = top;
-        top = top.next;
-        return temp.value;
-    }
 }
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -15,66 +15,7 @@ public class StackTest {
     }
 
     @Test
-    void isEmptyReturnsTrueIfEmpty() {
-
-        assertTrue(stack.isEmpty());
+    void stackInti() {
+        assertNotNull(stack);
     }
-
-    @Test
-    void isEmptyReturnsFalseIfStackIsNotEmpty() {
-
-        stack.push("John");
-        assertFalse(stack.isEmpty());
-    }
-
-    @Test
-    void peekThrowsStackEmptyExceptionIfStackIsEmpty() {
-        Throwable exception = assertThrows(StackEmptyException.class, () ->
-                stack.peek());
-
-        assertEquals("Stack is Empty!", exception.getMessage());
-    }
-
-    @Test
-    void peekReturnsItemInStack() throws StackEmptyException {
-        stack.push(5);
-        assertEquals(5, stack.peek());
-    }
-
-    @Test
-    void popReturnsItemInStack() {
-        stack.push(3.14);
-        assertEquals(3.14, stack.pop());
-    }
-
-    @Test
-    void popReturnsNullIfEmptyh() {
-        assertNull(stack.pop());
-    }
-
-    @Test
-    void peekThrowsStackEmptyExceptionAfterPop() {
-        stack.push("Leeroy");
-        stack.pop();
-
-        Throwable exception = assertThrows(StackEmptyException.class, () ->
-                stack.peek());
-
-        assertEquals("Stack is Empty!", exception.getMessage());
-
-    }
-
-    @Test
-    void push3ItemsPopEqualsLastItemPeekEquals2ndItem() throws StackEmptyException {
-        stack.push(42);
-        stack.push("Jenkins");
-        stack.push(5.5);
-
-        var resultPop = stack.pop();
-        var resultPeek = stack.peek();
-        assertEquals(5.5, resultPop);
-        assertEquals("Jenkins", resultPeek);
-
-    }
-
 }
